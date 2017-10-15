4d403ece157fe1a85b5db5f4f825c8c647f48934
C.y <yangruihan@vip.qq.com>
2017-10-15 10:33:21 +0800

    初始化项目

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,28 @@
+﻿using Entitas;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+
+    private Contexts _contexts;
+    private Systems _systems;
+
+    void Start()
+    {
+        _contexts = Contexts.sharedInstance;
+
+        _systems = CreateSystems(_contexts);
+        _systems.Initialize();
+    }
+
+    void Update()
+    {
+        _systems.Execute();
+    }
+
+    Systems CreateSystems(Contexts contexts)
+    {
+        return new Feature("Game")
+            ;
+    }
+}
